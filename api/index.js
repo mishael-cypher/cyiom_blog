bac7bb189f535785b3703f08a20e3832a1219da2
Mishael <somtookonkwo96.gmail.com>
2022-02-01 23:11:41 +0100

    finished writing the routes and implementing multer

--- api/index.js
+++ api/index.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose')
 const multer = require('multer')
 const dotenv = require('dotenv')
 
+// Routes
+authRoute = require('./routes/auth')
+userRoute = require('./routes/users')
+postRoute = require('./routes/posts')
+categoryRoute = require('./routes/categories')
+
 dotenv.config()
 
 
 const app = express()
 
-app.use()
+app.use(express.json())
+app.use('api/auth', authRoute)
+app.use('api/users', userRoute)
+app.use('api/posts', postRoute)
+app.use('api/categories', categoryRoute)
+
 
 mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
@@ -17,5 +28,21 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(console.log('Connected to Database'))
 .catch(err => console.error(err.message))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filenmae: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage: storage})
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json('file has been uploaded')
+})
+
 
 app.listen('5000', () => console.log('listening on port 5000'))
